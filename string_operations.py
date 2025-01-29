dd0ab23f2563d7e6f91e3b68b770c416e46f90c7
luccacampagnani <lcampagnani.ieu2023@student.ie.edu>
2025-01-29 17:13:39 +0100

    string operations examples

--- string_operations.py
+++ string_operations.py
@@ -6,4 +6,4 @@ print("hello")
 s1 = "hello"
 s2 = 4*s1 # you can only multiply by positive integer!!
 print(s2)
-print((4//2)*s1) # 4/2 is not an integer, so you use //
+print((4//2)*s1) # integer division, result will always be an integer, so you use //
